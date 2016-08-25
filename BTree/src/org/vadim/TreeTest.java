7d99deb0e0f43105300e5199c5799422de465b00
ovchi <ovchi@192.168.1.4>
2016-08-25 18:24:58 +0300

    try 1

--- BTree/src/org/vadim/TreeTest.java
+++ BTree/src/org/vadim/TreeTest.java
@@ -1,5 +1,6 @@
 package org.vadim;
 
+import java.util.Random;
 import java.util.concurrent.*;
 import java.util.concurrent.locks.*;
 import org.openjdk.jmh.annotations.Benchmark;
@@ -9,6 +10,7 @@ import org.openjdk.jmh.annotations.Measurement;
 import org.openjdk.jmh.annotations.Mode;
 import org.openjdk.jmh.annotations.OutputTimeUnit;
 import org.openjdk.jmh.annotations.Scope;
+import org.openjdk.jmh.annotations.Setup;
 import org.openjdk.jmh.annotations.State;
 import org.openjdk.jmh.annotations.Warmup;
 import org.openjdk.jmh.runner.Runner;
@@ -19,16 +21,16 @@ import org.openjdk.jmh.runner.options.OptionsBuilder;
 
 
 
-class Node implements NodeInterface{
+class NodeTest implements NodeInterface{
 	public int keyNum;
 	public int[] key;
 	public boolean leaf;
-	public Node[] children;
-	public Node(int keyNum,boolean leaf,int t){
+	public NodeTest[] children;
+	public NodeTest(int keyNum,boolean leaf,int t){
 		this.keyNum=keyNum;
 		this.leaf=leaf;
 		this.key=new int[2*t-1];
-		this.children=new Node[2*t];
+		this.children=new NodeTest[2*t];
 	}
 }
 
@@ -37,10 +39,10 @@ class Node implements NodeInterface{
  * Посчитал это лучшим способом реализовать особеннсоти функции поиска
  * (Нужно возвращать два значения одновременно при удачном нахождении)
  */
-class NodeIndex implements NodeInterface{
-	private Node node;
+class NodeIndexTest implements NodeInterface{
+	private NodeTest node;
 	private int index;
-	public NodeIndex(Node node,int index){
+	public NodeIndexTest(NodeTest node,int index){
 		this.node=node;
 		this.index=index;
 	}
@@ -51,15 +53,16 @@ class NodeIndex implements NodeInterface{
 	}
 }
 
+@State(Scope.Thread)
 @BenchmarkMode(Mode.AverageTime)
 @OutputTimeUnit(TimeUnit.NANOSECONDS)
 @Warmup(iterations = 5, time = 1, timeUnit = TimeUnit.SECONDS)
 @Measurement(iterations = 5, time = 1, timeUnit = TimeUnit.SECONDS)
-@Fork(5)
-public class Tree {
+@Fork(1)
+public class TreeTest {
 	private static final int t=100;
 	private int height=0;
-	public Node root;
+	public NodeTest root;
 	private ExecutorService search=Executors.newFixedThreadPool(4);//Потоки для поиска
 	private ExecutorService insert=Executors.newFixedThreadPool(1);
 	/*
@@ -67,14 +70,23 @@ public class Tree {
 	 * (они будут мешать друг другу)
 	 */
 	ReadWriteLock readWriteLock = new ReentrantReadWriteLock();
-	public Tree(){
-		this.root=new Node(0,true,t);
+
+	@Setup
+    public void setup() {
+		root=new NodeTest(0,true,t);
+		for (int j = 1; j < 1000000; j++) {// В цикле добавляю данные
+			this.treeInsert(j);
+		}
+    }
+
+	public TreeTest(){
+		this.root=new NodeTest(0,true,t);
 	}
 
 
-	public void treeSplitChild(Node x,int i){
-		Node y=x.children[i];
-		Node z=new Node(t-1,y.leaf,t);
+	public void treeSplitChild(NodeTest x,int i){
+		NodeTest y=x.children[i];
+		NodeTest z=new NodeTest(t-1,y.leaf,t);
 		for(int j=0;j<t-1;j++){
 			z.key[j]=y.key[j+t];
 			y.key[j+t]=0;
@@ -98,16 +110,18 @@ public class Tree {
 		x.keyNum++;
 	}
 
-
-
-
 	@Benchmark
+	public void InsertNew(){
+		treeInsert(1000);
+	}
+
+
+
 	public void treeInsert(int key){//При вызове этой функции задача добавляется в пул потоков для добавления
 		Runnable obj=()-> {
-			long time=System.nanoTime();
 			readWriteLock.writeLock().lock();//Надеюсь это работает так как написано в интернете
 			if(root.keyNum==2*t-1){
-				Node s=new Node(0,false,t);
+				NodeTest s=new NodeTest(0,false,t);
 				s.children[0]=root;
 				root=s;
 				height++;
@@ -117,12 +131,11 @@ public class Tree {
 			else
 				treeInsertNonfull(root,key);
 			readWriteLock.writeLock().unlock();
-			System.out.println("Insert ms="+(System.nanoTime()-time)/Math.pow(10, 6)+"\n");
 		};
 		insert.submit(obj);
 	}
 
-	public void treeInsertNonfull(Node x,int key){
+	public void treeInsertNonfull(NodeTest x,int key){
 		int i=x.keyNum-1;
 
 		if(x.leaf){
@@ -153,32 +166,33 @@ public class Tree {
 		}
 	}
 
-
+	@Benchmark
+	public void search(){
+		treeSearch(222);
+	}
 	public Future<NodeInterface> treeSearch(int key){//При вызове этой функции задача добавляется в пул потоков для поиска
 		Callable<NodeInterface> obj=()-> {
-					long time=System.nanoTime();
 					readWriteLock.readLock().lock();//Опять же, надеюсь это работает так как написано в интернете
 					NodeInterface objNode=treeSearch(root,key);
 					readWriteLock.readLock().unlock();
-					System.out.println("Search ms="+(System.nanoTime()-time)/Math.pow(10, 6)+"\n");
 					return objNode;
 				};
 		return search.submit(obj);
 	}
 
-	@Benchmark
-	public NodeInterface treeSearch(Node x,int key){
+
+	public NodeInterface treeSearch(NodeTest x,int key){
 		int i=0;
 
 		while(i<x.keyNum && key>x.key[i])
 			i++;
 		if(i<x.keyNum && key==x.key[i]){
-			NodeIndex objBuf=new NodeIndex(x,i);
+			NodeIndexTest objBuf=new NodeIndexTest(x,i);
 			return objBuf;
 		}
 		else if(x.leaf)
 		{
-			NodeIndex objBuf=new NodeIndex(null,0);
+			NodeIndexTest objBuf=new NodeIndexTest(null,0);
 			return objBuf;
 		}
 		else
@@ -187,7 +201,7 @@ public class Tree {
 
 	public static void main(String[] args) throws RunnerException {
         Options opt = new OptionsBuilder()
-                .include(Tree.class.getSimpleName())
+                .include(TreeTest.class.getSimpleName())
                 .build();
 
         new Runner(opt).run();
